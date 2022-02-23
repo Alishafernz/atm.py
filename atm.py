81d6216e19fac00952283e9716727da650670ea8
Alishafernz <78721993+Alishafernz@users.noreply.github.com>
2022-02-23 15:13:23 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,13 @@
+Atm.py
+def__init__(self atm card,atm number, pin number,mobile number)
+  self.atm card = atm card
+  self.atm number = atm number
+  self.pin number = pin number
+  self.pin number = mobile number
+
+def start(self):
+  print("cash with drawal")
+
+def stop("self"):
+  print("Blance with enquiry")
+  
